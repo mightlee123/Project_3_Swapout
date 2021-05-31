9f5cbb5482589d288a4d4a2cccfeb9ae9f252cd3
Aye <danielayechan310@gmail.com>
2021-05-31 00:18:56 -0700

    updated UI file

--- User_Interface.py
+++ User_Interface.py
@@ -3,10 +3,11 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Any
 
-
 st.title("Welcome to Swapout!")
+
 st.markdown("## Your customized virtual market place")
 
+#background picture
 st.markdown(
     """
     <style>
@@ -28,39 +29,22 @@ import os.path
 # # create a button in the side bar that will move to the next page/radio button choice
 # next = st.sidebar.button('Next on list')
 
-# will use this list and next button to increment page, MUST BE in the SAME order
-# as the list passed to the radio button
 new_choice = ['Home','Gallery','About']
-
-# This is what makes this work, check directory for a pickled file that contains
-# the index of the page you want displayed, if it exists, then you pick up where the
-#previous run through of your Streamlit Script left off,
-# if it's the first go it's just set to 0
 if os.path.isfile('next.p'):
     next_clicked = pkle.load(open('next.p', 'rb'))
-    # check if you are at the end of the list of pages
     if next_clicked == len(new_choice):
-        next_clicked = 0 # go back to the beginning i.e. homepage
+        next_clicked = 0 
 else:
     next_clicked = 0 #the start
 
-# this is the second tricky bit, check to see if the person has clicked the
-# next button and increment our index tracker (next_clicked)
 if next:
-    #increment value to get to the next page
     next_clicked = next_clicked +1
-
-    # check if you are at the end of the list of pages again
     if next_clicked == len(new_choice):
         next_clicked = 0 # go back to the beginning i.e. homepage
 
-# create your radio button with the index that we loaded
 choice = st.sidebar.radio("go to",('Home', 'Gallery', 'About'), index=next_clicked)
-
-# pickle the index associated with the value, to keep track if the radio button has been used
 pkle.dump(new_choice.index(choice), open('next.p', 'wb'))
 
-# finally get to whats on each page
 if choice == 'Home':
     st.write('this is home')
 elif choice == 'Gallery':
@@ -69,7 +53,6 @@ elif choice == 'About':
     st.write('About page')
 
 left_column, right_column = st.beta_columns(2)
-# You can use a column just like st.sidebar:
 if left_column.button('New User'):
     input_data = st.text_input("Create User ID")    
     input_data = st.text_input("Enter personal number")
